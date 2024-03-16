a767e8ffa86e635b23445ac97e50b85206efd389
Nathaniel <nathaniel.josse@gmail.com>
2024-03-16 22:37:26 +0100

    Filters added. Page is now refreshing after data update

--- server.js
+++ server.js
@@ -31,27 +31,39 @@ app.get('/', async (req, res) => {
 app.post('/', async (req, res) => {
     let film = await Film.deleteMany(); // on supprime tous les documents de la collection pour update
 
-    const filmXlsx = xlsx.parse('assets/table/film.xlsx'); // on récupère le fichier excel
+    const filmXlsx = await xlsx.parse('assets/table/film.xlsx'); // on récupère le fichier excel
     const filmData = filmXlsx[0].data; // on récupère les données du fichier excel
-    // let filmStock = new Film();
-    filmData.forEach(data => {
-        console.log(data);
-        if (data[0] === 'Id') return;
-        let film = new Film({
-            id: data[0],
-            titre: data[1],
-            titre_original: data[2],
-            realisateur: data[3],
-            annee: data[4],
-            nationalite: data[5],
-            duree: data[6],
-            genre: data[7],
-            synopsis: data[8]
-        });
-        film.save();
-    });
-
-    //Lien à MongoDB, et update
+    let filmStock = new Film(); // On va mettre en place un stock pour les films ayant plusieurs réalisateurs
+    for await (const data of filmData) {
+        if (data[0] !== 'Id'){ // on ignore la première ligne du fichier excel
+            let currentFilm = new Film({
+                id: data[0],
+                titre: data[1],
+                titre_original: data[2],
+                realisateur: data[3],
+                annee: data[4],
+                nationalite: data[5],
+                duree: data[6],
+                genre: data[7],
+                synopsis: data[8]
+            });
+            // FILTERS //
+            // on enlève les balises html
+            currentFilm.synopsis = currentFilm.synopsis.replace(/<[^>]*>?/gm, '');
+            // Si le titre est le même, c'est que le film a plusieurs réalisateurs
+            if (currentFilm.titre === filmStock.titre) {
+                currentFilm.realisateur = filmStock.realisateur + ', ' + currentFilm.realisateur;
+                currentFilm.id = filmStock.id;
+            // END OF FILTERS //
+            } else {
+                filmStock.save();
+            }
+            // on met à jour le film courant
+            filmStock = currentFilm;
+        }
+    };
+    console.log("MàJ Done !");
+    res.redirect('/');
 });
 
 app.listen(2024, () => {
